246c7a610f1294b523a6e275d5f309490fce85f6
Tomm223 <dan.osipov99@mail.ru>
2023-01-14 15:15:35 +0300

    fix imorts

--- src/modules/ChatNav/components/LinkToProfile/index.tsx
+++ src/modules/ChatNav/components/LinkToProfile/index.tsx
@@ -1,6 +1,6 @@
 import CompileMaster from "@/core/CompileJSX"
 import styles from './styles.module.scss'
-import Polygon1 from '@/static/icons/polygon 1.png'
+import Polygon1 from '@/static/icons/Polygon1.png'
 
 interface LinkToProfile {
    onClick?: () => void
