1e0b76caa19a8d2517043a234b30be510035c8a6
Tomm223 <dan.osipov99@mail.ru>
2023-02-27 17:36:07 +0300

    add log for state

--- src/store/Store.ts
+++ src/store/Store.ts
@@ -75,7 +75,7 @@ export default class Store extends EventBus {
 
     // localStorage.removeItem(Store.STORE_NAME)
     const savedState = localStorage.getItem(Store.STORE_NAME)
-    // console.log('saves_state', savedState)
+    console.log('saves_state', savedState)
     this._state = savedState ? JSON.parse(savedState) ?? Initialstate : Initialstate
     // this._state = Initialstate
 
