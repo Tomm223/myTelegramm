fc562d95c767cdf0b5f74bf679ffefb626876b18
Tomm223 <dan.osipov99@mail.ru>
2023-02-27 15:55:42 +0300

    merge sprint_3 ^6

--- src/widgets/Profile/UI/ProfileForm/constants.ts
+++ src/widgets/Profile/UI/ProfileForm/constants.ts
@@ -1,9 +1,6 @@
 import { StateForm, ValidateForm } from '@/shared/form/FormConstructor/types'
 import { ValidatesForm } from '@/shared/form/FormConstructor/validates.reg'
-<<<<<<< HEAD
-=======
 import { UserType } from '@/types/user'
->>>>>>> sprint_3
 
 export const ProfileState: { data: StateForm; password: StateForm } = {
   data: {
@@ -34,82 +31,44 @@ export const ProfileValidate: { data: ValidateForm; password: ValidateForm } = {
   },
 }
 
-<<<<<<< HEAD
-export const PersonForm = (isEdit: boolean) => ({
-  state: ProfileState.data,
-<<<<<<< HEAD
-  validate: ProfileValidate.data, //{ ...ProfileValidate.data, ...ProfileValidate.password },
-=======
 export const PersonForm = (isEdit: boolean, user: UserType) => ({
   state: ProfileState.data,
   validate: ProfileValidate.data,
->>>>>>> sprint_3
-=======
-  validate: ProfileValidate.data,
->>>>>>> 202543e185edbb6c76121c6c5c22a173cfe03d8a
   inputs: [
     {
       isEdit: isEdit,
       label: 'Почта',
       name: 'email',
-<<<<<<< HEAD
-      text: 'katenkadem1denko@yandex.ru',
-      type: 'text',
-    },
-    { isEdit: isEdit, label: 'Логин', name: 'login', text: 'Dancsgo', type: 'text' },
-=======
       text: user.email,
       type: 'text',
     },
     { isEdit: isEdit, label: 'Логин', name: 'login', text: user.login, type: 'text' },
->>>>>>> sprint_3
     {
       isEdit: isEdit,
       label: 'Имя',
       name: 'first_name',
-<<<<<<< HEAD
-      text: 'Daniil',
-=======
       text: user.first_name,
->>>>>>> sprint_3
       type: 'text',
     },
     {
       isEdit: isEdit,
       label: 'Фамилия',
       name: 'second_name',
-<<<<<<< HEAD
-      text: 'Osipov',
-=======
       text: user.second_name,
->>>>>>> sprint_3
       type: 'text',
     },
     {
       isEdit: isEdit,
       label: 'Имя в чате',
-<<<<<<< HEAD
-<<<<<<< HEAD
-      name: 'name',
-=======
-      name: 'display_name',
->>>>>>> 202543e185edbb6c76121c6c5c22a173cfe03d8a
-      text: 'Dannial`',
-=======
       name: 'display_name',
       text: user.display_name,
->>>>>>> sprint_3
       type: 'text',
     },
     {
       isEdit: isEdit,
       label: 'Телефон',
       name: 'phone',
-<<<<<<< HEAD
-      text: '89534545',
-=======
       text: user.phone,
->>>>>>> sprint_3
       type: 'number',
     },
   ],
@@ -117,49 +76,27 @@ export const PersonForm = (isEdit: boolean, user: UserType) => ({
 
 export const PasswordForm = (isEdit: boolean) => ({
   state: ProfileState.password,
-<<<<<<< HEAD
-<<<<<<< HEAD
-  validate: ProfileValidate.password, //{ ...ProfileValidate.data, ...ProfileValidate.password },
-=======
   validate: ProfileValidate.password,
->>>>>>> sprint_3
-=======
-  validate: ProfileValidate.password,
->>>>>>> 202543e185edbb6c76121c6c5c22a173cfe03d8a
   inputs: [
     {
       isEdit: isEdit,
       label: 'Старый пароль',
-<<<<<<< HEAD
-      name: 'old_password',
-      text: 'Osipov',
-=======
       name: 'oldPassword',
       text: '',
->>>>>>> sprint_3
       type: 'text',
     },
     {
       isEdit: isEdit,
       label: 'Новый пароль',
-<<<<<<< HEAD
-      name: 'password',
-      text: 'Dannial`',
-=======
       name: 'newPassword',
       text: '',
->>>>>>> sprint_3
       type: 'password',
     },
     {
       isEdit: isEdit,
       label: 'Повторите Пароль',
       name: 'repeat_password',
-<<<<<<< HEAD
-      text: 'Dannial',
-=======
       text: '',
->>>>>>> sprint_3
       type: 'password',
     },
   ],
