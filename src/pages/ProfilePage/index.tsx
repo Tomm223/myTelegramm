c41db0a33ce84799246dd301a93e215cbd33c485
Tomm223 <dan.osipov99@mail.ru>
2023-02-27 15:43:07 +0300

    merge witch sprint_3 ^2

--- src/pages/ProfilePage/index.tsx
+++ src/pages/ProfilePage/index.tsx
@@ -18,11 +18,7 @@ export default class ProfilePage extends Component<ProfileType> {
   protected render(): HTMLElement {
     return (
       <div class={styles.container}>
-<<<<<<< HEAD
-        <div class={styles.link_back}>{new LinkToBack({ href: '/messanger' }).getContent()}</div>
-=======
         <div class={styles.link_back}>{new LinkToBack({ href: '/messenger' }).getContent()}</div>
->>>>>>> sprint_3
         {this.childrenHTML.elements.profile}
       </div>
     )
--- src/pages/SingUpPage/index.tsx
+++ src/pages/SingUpPage/index.tsx
@@ -1,10 +1,7 @@
 import Component from '@/core/Component'
 import SingUp from '@/widgets/SingUp'
 import CompileMaster from '@/core/CompileJSX'
-<<<<<<< HEAD
-=======
 import { SingController } from '@/service/sing.service'
->>>>>>> sprint_3
 
 interface SingUnPageType {
   modal?: Component
@@ -12,27 +9,16 @@ interface SingUnPageType {
 
 export default class SingUnPage extends Component<SingUnPageType> {
   constructor(props: SingUnPageType) {
-<<<<<<< HEAD
-    props.modal = new SingUp({
-      size: { width: '340px', height: '615px' },
-      onSubmit: (form) => {},
-=======
     const controller = new SingController()
 
     props.modal = new SingUp({
       size: { width: '340px', height: '615px' },
       onSubmit: controller.registration,
->>>>>>> sprint_3
     })
     super(props)
   }
 
   protected render(): HTMLElement {
-<<<<<<< HEAD
-    console.log('page render')
-
-=======
->>>>>>> sprint_3
     return <div>{this.childrenHTML.elements.modal}</div>
   }
 }
